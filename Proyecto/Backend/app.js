256c6041ed3915d2e155eaa41dc7c7552c019d8a
Astrid Monserrath Echeagaray Aceves <127809820+AstridEcheagaray@users.noreply.github.com>
2024-04-25 10:51:35 -0700

    Conexion a mySQL

--- Proyecto/Backend/app.js
+++ Proyecto/Backend/app.js
@@ -6,10 +6,39 @@ let app = express();
 app.use(express.json);
 app.use(cors());
 
+//CONEXION A MYSQL - cadena de conexion
+let conexion = mysql.createConnection({
+    host: '127.0.0.1', 
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//NOS CONECTAMOS A MySQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
 //RUTAS DE ACCESO
 app.get("/", function(req,res){
     res.send("Ruta de inicio")
 })
+
+//SELECCIONAMOS TODOS LOS CLIENTES
+app.get('/api/clientes', (req,res)=>{
+    conexion.query('SELLECT * FROM clientes', (error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
 
 //ENCENDER EL SERVIDOR
 let puerto = 3000;
