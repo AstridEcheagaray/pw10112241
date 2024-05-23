6ac44e7f112c6c6d7807331fc5a4c02cba50452e
Astrid Echeagaray <127809820+AstridEcheagaray@users.noreply.github.com>
2024-05-23 10:55:30 -0700

    Pantalla Registro

--- proyecto/frontend/tienda/src/main.js
+++ proyecto/frontend/tienda/src/main.js
@@ -4,8 +4,31 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// COMIENZA FireBase
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVfqN2ds9dccZZh7AjmDrgyEUpan7Y44w",
+  authDomain: "programacionweb-8595a.firebaseapp.com",
+  projectId: "programacionweb-8595a",
+  storageBucket: "programacionweb-8595a.appspot.com",
+  messagingSenderId: "707625682561",
+  appId: "1:707625682561:web:791acdacb91da37bbb2f80"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+//TERMINA Firebase
+
+
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
 
 app.mount('#app')
